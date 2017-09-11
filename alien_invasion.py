c92e5851a9c2ec982593e93a5eae851014b1af4d
Allen Ball <qiujunya@gmail.com>
2017-09-11 18:42:26 +0800

    add bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     # 初始化并创建一个屏幕对象
@@ -13,14 +14,20 @@ def run_game():
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
 
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
     # 设置背景色
     bg_color = (230, 230, 230)
 
     # 开始游戏的主循环
     while True:
         #监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        # 删除已消失的子弹
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
